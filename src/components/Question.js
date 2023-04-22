2172f7263965dc499cd22ee7190af553f8652c51
Sang Kibet Elkana <elkanahkibet6@gmail.com>
2023-04-22 16:46:00 +0300

    Done

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,27 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+
+  useEffect(() => {
+    if (timeRemaining ===0){
+      setTimeRemaining(10);
+      onAnswered(false)
+      return;
+    }
+
+    //this uses setTimeout to run a callback function after 1 second.
+    const timerId = setTimeout(() => {
+      
+      setTimeRemaining((timeRemaining) => timeRemaining - 1);
+    }, 1000); //decrements the time by 1 second.
+    //
+    return function cleanup () {
+      clearTimeout(timerId);
+    };
+  },[timeRemaining,onAnswered]);
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
